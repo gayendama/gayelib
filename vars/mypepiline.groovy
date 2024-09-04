e285ef6d3e26b25f3f7023b47cf8871732e7e29f
gayendama <117781785+gayendama@users.noreply.github.com>
2024-09-04 12:37:11 +0000

    Update mypepiline.groovy

--- vars/mypepiline.groovy
+++ vars/mypepiline.groovy
@@ -21,7 +21,7 @@ pipeline {
         stage('Build image docker') {
             steps {
                 script {
-                    buildDockerImage(p.IMAGE_NAME, p.DOCKER_ID)
+                    build(p.IMAGE_NAME, p.DOCKER_ID)
                 }
             }
         }
